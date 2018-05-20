2c21c9a4bbd22dab94bd89f06858a01a61b03a51
Takahiko Kawasaki <taka@authlete.com>
2018-05-20 22:01:09 +0900

    Removed a comment from TokenController.

--- rsc/TokenController.php
+++ rsc/TokenController.php
@@ -1,10 +1,11 @@
 <?php
 namespace _NAMESPACE_;
 
+
 use Authlete\Laravel\Controller\DefaultTokenController;
+
 
 class TokenController extends DefaultTokenController
 {
-    // Override getSpi() method as necessary.
 }
 ?>
