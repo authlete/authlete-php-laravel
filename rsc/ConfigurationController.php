a52225813b7b5e6e39ec4b50c4de6664c8a7c9c6
Takahiko Kawasaki <taka@authlete.com>
2018-05-20 22:05:24 +0900

    Added some controller classes under the 'rsc' directory.

---
+++ rsc/ConfigurationController.php
@@ -0,0 +1,11 @@
+<?php
+namespace _NAMESPACE_;
+
+
+use Authlete\Laravel\Controller\DefaultConfigurationController;
+
+
+class ConfigurationController extends DefaultConfigurationController
+{
+}
+?>
