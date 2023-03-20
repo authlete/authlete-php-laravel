98118454b0a783f5a30416b310b8979911f02db9
Maxime Longuepe <maxime.longuepe@authlete.com>
2023-03-20 17:48:15 +0900

    Handling ticket must be a string error

--- rsc/authorization.blade.php
+++ rsc/authorization.blade.php
@@ -1,3 +1,9 @@
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <html>
 <head>
   <meta charset="UTF-8">
